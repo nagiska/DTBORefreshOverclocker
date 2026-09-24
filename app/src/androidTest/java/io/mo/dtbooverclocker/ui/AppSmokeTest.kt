c378c373fae23c55d62be5193123bc57ba9ae507
nagiska <nagiska@users.noreply.github.com>
2026-09-24 17:11:23 +0800

    test: 补 performScrollTo import

--- app/src/androidTest/java/io/mo/dtbooverclocker/ui/AppSmokeTest.kt
+++ app/src/androidTest/java/io/mo/dtbooverclocker/ui/AppSmokeTest.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.test.onAllNodesWithText
 import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollTo
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
